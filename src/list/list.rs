93afdbdc3a4c572a8395a9b2ff9bda0e3d827477
maliByatzes <malib2027@gmail.com>
2023-12-04 15:41:01 +0200

    start ls cmd

---
+++ src/list/list.rs
@@ -0,0 +1,45 @@
+use std::fs;
+
+use lexopt::prelude::*;
+
+struct Args {
+    directory: String,
+}
+
+fn parse_args() -> Result<Args, lexopt::Error> {
+    let mut directory = None;
+    let mut parser = lexopt::Parser::from_env();
+
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Value(val) if directory.is_none() => {
+                directory = Some(val.string()?);
+            } 
+            _ => return Err(arg.unexpected()),
+        }
+    }
+
+    Ok(Args {
+        directory: directory.ok_or("Missing argument [DIRECTORY]")?,
+    })
+}
+
+fn list_files(directory: &str) -> Result<(), Box<dyn std::error::Error>> {
+    for entry in fs::read_dir(directory)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            println!("{}/", path.display());
+        } else {
+            println!("{}", path.display());
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = parse_args()?;
+    list_files(&args.directory)?;
+    Ok(())
+}
